596648968d2b63568f625fedc61d73169fca01dc
Kuda <kudan69@gmail.com>
2017-06-29 13:33:07 -0400

    Moving view logic from model to helper Also getting rid of bug where scoring was being done even when needed

--- app/helpers/attempts_helper.rb
+++ app/helpers/attempts_helper.rb
@@ -1,2 +1,17 @@
 module AttemptsHelper
+
+  def score_attempt attempt
+    correct = 0
+    total = 0
+
+    # default 0/0 score for drills that don't report a grade
+    unless attempt.responses.blank?
+      correct = attempt.correct
+      total = attempt.total
+    end
+
+    # raw '<span class="score"><span class="correct">' + correct + '</span>/<span class="total">' + total + '</span></span>'
+    raw "<span class='score'><span class='correct'>#{correct.to_s}</span>/<span class='total'>#{total.to_s}</span></span>"
+  end
+
 end
