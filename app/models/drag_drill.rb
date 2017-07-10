6dcf1ca5fa2895adafaf40b82489714dbc5fac4b
Kuda <kudan69@gmail.com>
2017-07-10 10:42:33 -0400

    Fix: Now conditionally fetching attempts

--- app/models/drag_drill.rb
+++ app/models/drag_drill.rb
@@ -32,7 +32,7 @@ class DragDrill < Drill
   end
 
   def response_order current_user
-    current_user.attempts.last.responses.map{|r| r.exercise_item_id.to_i}
+    current_user.attempts.last.responses.map{|r| r.exercise_item_id.to_i} unless current_user.nil?
   end
 
 end
