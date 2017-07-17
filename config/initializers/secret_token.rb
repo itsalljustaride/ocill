b125555034d9bc72fde726b844201b7e8b9b450f
Johnathon Beals <johnathb@lrcm-johnathb.local>
2017-07-17 16:52:29 -0400

    changed secret token to secret key base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ocill::Application.config.secret_token = '10223a860bddc1618bd85c8fd1ad5255a410b3ce508853e138b17c00babda526a3fab84fb73746175756b716625fa11f8cf19004146854979afb0aedee7a5df0'
+Ocill::Application.config.secret_key_base = '10223a860bddc1618bd85c8fd1ad5255a410b3ce508853e138b17c00babda526a3fab84fb73746175756b716625fa11f8cf19004146854979afb0aedee7a5df0'
