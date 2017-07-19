26cf1c0051c5d4a1adf78f36180097330d138c8d
Johnathon Beals <johnathb@lrcm-johnathb.local>
2017-07-19 15:26:33 -0400

    fixed some deprecated find_or_create methods

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,10 @@ Ocill::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+  }
 
   config.eager_load = true
 
