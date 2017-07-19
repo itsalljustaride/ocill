ac0e7ef1e56aeb878c716f336f58a546ffbd3b00
Johnathon Beals <johnathb@lrcm-johnathb.local>
2017-07-19 11:02:18 -0400

    fixed some deprecated find_or_create methods

--- app/models/launch.rb
+++ app/models/launch.rb
@@ -107,11 +107,7 @@ class Launch
     role = lti_roles_to_ocill_user_role(roles)
     email = "user#{rand(10000..999999999999).to_s}@example.com"
     password = "pass#{rand(10000..999999999999).to_s}"
-    u = User.find_or_create_by(lti_user_id: user_id, role: role, email: email)
-    u.password = password
-    u.save!
-    
-    return u
+    u = User.find_or_create_by_lti_user_id(lti_user_id: user_id, role: role, email: email, password: password)
   end
 
   def find_or_create_child_section(context_id, custom_canvas_course_id)
@@ -153,7 +149,7 @@ class Launch
   
   def find_activity
     return nil unless section
-    Activity.find_or_create_by(lti_resource_link_id: params[:resource_link_id], section_id: section.id)
+    Activity.find_or_create_by_lti_resource_link_id(lti_resource_link_id: params[:resource_link_id], section_id: section.id)
   end
 
   def canvas_course_id
