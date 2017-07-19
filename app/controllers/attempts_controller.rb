f530ea6c2337cdd93660e44345583813941e8ebf
Johnathon Beals <johnathb@lrcm-johnathb.local>
2017-07-19 17:13:22 -0400

    debugging score post error

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -39,8 +39,8 @@ class AttemptsController < InheritedResources::Base
         if current_user.is_lti?
           # If there is no active tool, get it out of the session
           @tool = @tool || Rails.cache.fetch(session[:launch_tool_cache_key])
-          score = Float::NaN
-          until score != Float::NaN
+          score = Float::NAN
+          until score != Float::NAN
             score = @attempt.decimal_score
           end
           if @tool && @tool.outcome_service?
