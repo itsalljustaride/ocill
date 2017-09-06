583dd4492daf999c77cf63f045a4cb116af1f185
Johnathon Beals <johnathb@lrcm-johnathb.local>
2017-09-06 14:14:30 -0400

    making kplayer uuid more randomy

--- app/assets/javascripts/exercises.js
+++ app/assets/javascripts/exercises.js
@@ -1,7 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-function _embedVideo(targetId,wid,uiconf_id,flashvars,entry_id,cb){
+function embedKalturaVideo(targetId,wid,uiconf_id,flashvars,entry_id,cb){
     kWidget.embed({
         targetId: targetId,
         wid: wid,
